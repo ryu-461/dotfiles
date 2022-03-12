7c899507f6f76b5fac7b6cfb5e51cc5de050cbf3
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-03-12 22:46:43 +0900

    update: Update line breaks & colors

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ COLOR_GRAY="\033[1;38;5;243m"
 COLOR_BLUE="\033[1;34m"
 COLOR_GREEN="\033[1;32m"
 COLOR_RED="\033[1;31m"
-COLOR_PURPLE="\033[1;35m"
 COLOR_YELLOW="\033[1;33m"
 COLOR_NONE="\033[0m"
 
@@ -20,8 +19,8 @@ info() {
 }
 
 headline() {
-  echo -e "${COLOR_GRAY}==============================${COLOR_NONE}"
-  echo -e "${COLOR_PURPLE}$1${COLOR_NONE}"
+  echo -e "\n${COLOR_GRAY}==============================${COLOR_NONE}"
+  echo -e "${COLOR_BLUE}$1${COLOR_NONE}"
   echo -e "${COLOR_GRAY}==============================${COLOR_NONE}"
 }
 
