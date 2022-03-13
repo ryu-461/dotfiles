2d95ac550d04ba9a966db0c2f74e5841afe5b12d
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-03-13 19:13:40 +0900

    update: Update messages

--- install-scripts/install-termux.sh
+++ install-scripts/install-termux.sh
@@ -4,38 +4,36 @@ set -ue
 
 # In order to have access to shared storage
 if [[ ! -d $HOME/dotfiles ]]; then
-  echo "Access to shared storage."
+  info "Access to shared storage."
   termux-setup-storage
-  echo "Done."
+  success "Setup done."
 fi
-echo ""
 
-echo "Updating the packages to the latest..."
-echo "Use Pkg."
-  pkg update
-  pkg install wget git curl proot vim -y
-echo "Done."
-echo ""
+# Update packages
+headline "Packages"
+info "Updating the packages to the latest..."
+pkg update
+pkg install wget git curl proot vim -y
+
 
 # Install anyenv
+headline "anyenv"
 if ! has "anyenv"; then
-    echo "Installing anyenv..."
-    git clone https://github.com/anyenv/anyenv ~/.anyenv
-    ~/.anyenv/bin/anyenv init
-    anyenv install --init
-    echo "Setting anyenv plugin..."
-    mkdir -p $(anyenv root)/plugins
-    git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+  info "Installing anyenv..."
+  git clone https://github.com/anyenv/anyenv ~/.anyenv
+  ~/.anyenv/bin/anyenv install --init
+  info "Setting anyenv plugin..."
+  mkdir -p ~/.anyenv/plugins
+  git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
 else
-  echo "anyenv is already installed."
+  success "anyenv is already installed."
 fi
-echo ""
 
 # Install Volta
+headline "Volta"
 if ! has "volta"; then
-  echo "Installing Volta..."
+  info "Installing Volta..."
   curl https://get.volta.sh | bash -s -- --skip-setup
 else
-  echo "Volta is already installed."
+  success "Volta is already installed."
 fi
-echo ""
