2a3d5017d7c785ef1e23f963107cf07fea19877a
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-07-17 17:42:36 +0900

    fix: Fix some functions

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,38 +9,32 @@ COLOR_BLUE="\033[1;34m"
 COLOR_YELLOW="\033[1;33m"
 COLOR_GREEN="\033[1;32m"
 
-_headline() {
+headline() {
   echo -e "\n${COLOR_GRAY}==============================${COLOR_NONE}"
   echo -e "${COLOR_BLUE}$1${COLOR_NONE}"
   echo -e "${COLOR_GRAY}==============================${COLOR_NONE}"
 }
-alias headline=_headline
 
-_run() {
+run() {
   echo -e "\n${COLOR_BLUE}▶ $1${COLOR_NONE}"
 }
-alias run=_run
 
-_info() {
+info() {
   echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
 }
-alias info=_info
 
-_warning() {
+warning() {
   echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1\n"
 }
-alias warning=_warning
 
-_error() {
+error() {
   echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
   exit 1
 }
-alias error=_error
 
-_success() {
+success() {
   echo -e "${COLOR_GREEN}$1${COLOR_NONE}\n"
 }
-alias success=_success
 
 DOT_BASE=$HOME/dotfiles
 DOT_TARBALL=https://github.com/ryu-461/dotfiles/tarball/main
