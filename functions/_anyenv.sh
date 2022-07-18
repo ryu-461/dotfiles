dd17315579f0ee339da152540a90cbe8f64f5319
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-07-18 15:09:13 +0900

    update: Replace HOME

--- functions/_anyenv.sh
+++ functions/_anyenv.sh
@@ -3,12 +3,12 @@ _anyenv() {
   if ! has "anyenv"; then
     info "Installing anyenv..."
     if [[ ! -d $HOME/.anyenv ]]; then
-      git clone https://github.com/anyenv/anyenv ~/.anyenv
+      git clone https://github.com/anyenv/anyenv $HOME/.anyenv
     fi
-    ~/.anyenv/bin/anyenv install --init
+    $HOME/.anyenv/bin/anyenv install --init
     info "Setting anyenv plugin..."
-    mkdir -p ~/.anyenv/plugins
-    git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
+    mkdir -p $HOME/.anyenv/plugins
+    git clone https://github.com/znz/anyenv-update.git $HOME/.anyenv/plugins/anyenv-update
   else
     success "anyenv is already installed."
   fi
