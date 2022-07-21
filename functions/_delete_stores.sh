c74e2ea5b1c22dad07c78d9974deb2431df6d9f5
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-07-21 23:38:17 +0900

    update: Add double quote

--- functions/_delete_stores.sh
+++ functions/_delete_stores.sh
@@ -1,4 +1,4 @@
 _delete_stores () {
-  sudo find $1 \( -name ".DS_Store" -or -name "._*" -or -name "Thumbs.db" -or -name "Desktop.ini" \) -delete -print;
+  sudo find "$1" \( -name ".DS_Store" -or -name "._*" -or -name "Thumbs.db" -or -name "Desktop.ini" \) -delete -print;
 }
 alias dss=_delete_stores
